3974564af6b0c75570cc0d9a6488102f07f5f425
Aaron <71030849+aprice5475@users.noreply.github.com>
2024-05-09 14:33:32 -0400

    finished outlines of ejs files

--- newsAppServer.js
+++ newsAppServer.js
@@ -15,6 +15,10 @@ const MONGO_URI = `mongodb+srv://${process.env.MONGO_DB_USERNAME}:${process.env.
 
 const API_URL = "https://newsapi.org/v2/everything?";
 
+app.set("views", path.resolve(__dirname, "templates"));
+app.set("view engine", "ejs");
+app.use(bodyParser.urlencoded({ extended: false }));
+
 async function getTopHeadlines() {
   // TODO:
 }
@@ -22,3 +26,28 @@ async function getTopHeadlines() {
 async function getAllNews() {
   //TODO:
 }
+
+app.get("/", (req, res) => {
+  res.redirect("index");
+});
+app.get("/index", (req, res) => {
+  res.render("index");
+});
+app.get("/search", (req, res) => {
+  res.render("searchForm");
+});
+app.post("/search", (req, res) => {
+  res.render("postSearchForm");
+});
+app.get("/register", (req, res) => {
+  res.render("registrationForm");
+});
+app.post("/register", (req, res) => {
+  res.render("postRegistration");
+});
+app.get("/login", (req, res) => {
+  res.render("login");
+});
+app.post("/login", (req, res) => {
+  res.redirect("/index");
+});
